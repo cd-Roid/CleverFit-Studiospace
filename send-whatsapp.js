17644d7474db1bc037ec227d173518c1ea58702c
George <giyawe98@gmail.com>
2020-06-01 17:51:18 +0200

    refactoreed the file

--- send-whatsapp.js
+++ send-whatsapp.js
@@ -1,20 +1,15 @@
 require('dotenv').config()
 const scraperResponse = require('./puppeteer')
+const client = require('twilio')( process.env.ACCOUNT_SID, process.env.AUTH_TOKEN)
 
 
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN;
-
-
-const client = require('twilio')( accountSid, authToken)
 async function sendMessage() {
-    
     try {
         client.messages.create({
-            from: 'whatsapp: +14155238886',
-            to: 'whatsapp:' +process.env.MY_PHONE_NUMBER,
-            body: scraperResponse
-        }).then(message=>console.log(scraperResponse))
+            from: 'whatsapp: +19168238709',
+            to: 'whatsapp: ' + process.env.MY_PHONE_NUMBER,
+            body: 'hello'
+        }).then(message => console.log(message.sid))
          
     } catch (error) {
         console.error(error);
