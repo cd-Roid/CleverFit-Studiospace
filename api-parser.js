1c4d250954c846bd7deb9fc7fbc28e265f31410e
George <giyawe98@gmail.com>
2020-06-02 15:50:47 +0200

    formatted outgoing Message and added the hourly request. Still waiting on the the twitter dev acount approval

--- api-parser.js
+++ api-parser.js
@@ -1,17 +1,19 @@
 const fetch = require("node-fetch").default;
 
 
-async function getResponsr() {
+setInterval(getResponse, 1000*10);
+
+async function getResponse() {
   try {
     let link = await fetch('http://www.clever-fit.com/de-de/fitnessstudio-in-der-naehe/clever-fit-gummersbach/?type=3002');
     let response = await link.json()
     let peopleInGym = response.data.response.countCheckedInCustomer
     let outMsg = `Es sind ${peopleInGym} Leute am trainieren.`
-    const fullMsg = 'Das FitnessStudio ist voll'
-    console.log(outMsg)
+    
+    console.log(outMsg);
+    module.exports = outMsg
   } catch (error) {
     console.error(error);
   }
 }
 
-getResponsr();
