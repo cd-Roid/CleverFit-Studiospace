57e531a9953a0c6b265451e1f2c3188bd7e320c5
cd-Roid <giyawe98@gmail.com>
2020-07-07 21:11:55 +0200

    removed es6 syntax

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv').config();
 
-export const consumer_key = process.env.TWITTER_API_KEY;
-export const consumer_secret = process.env.TWITTER_SECRET_KEY;
-export const access_token_key = process.env.TWITTER_ACCESS_TOKEN;
-export const access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
+const consumer_key = process.env.TWITTER_API_KEY;
+const consumer_secret = process.env.TWITTER_SECRET_KEY;
+const access_token_key = process.env.TWITTER_ACCESS_TOKEN;
+const access_token_secret = process.env.TWITTER_ACCESS_TOKEN_SECRET;
