f39a08aa04c1093e2ea45eedbf5c6045bbb52e1f
cd-Roid <giyawe98@gmail.com>
2020-07-07 20:45:00 +0200

    getResponse now returns outMsg as value.

--- api-parser.js
+++ api-parser.js
@@ -1,19 +1,15 @@
 const fetch = require("node-fetch").default;
 
 
-setInterval(getResponse, 1000 * 10);
-
-async function getResponse() {
+module.exports = async function getResponse() {
   try {
     let link = await fetch('http://www.clever-fit.com/de-de/fitnessstudio-in-der-naehe/clever-fit-gummersbach/?type=3002');
     let response = await link.json();
     let peopleInGym = response.data.loadNumberOfPresentCustomersInPercent;
     //Theres a max of 42 people allowed in the gym.
     let calculatePeople = Math.floor(peopleInGym * 42);
-
     let outMsg = `Es sind ${calculatePeople} Leute am trainieren.`;
-    console.log(outMsg);
-    module.exports = outMsg;
+    return outMsg;
   } catch (error) {
     console.error(error);
   }
