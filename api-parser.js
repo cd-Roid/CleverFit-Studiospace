ed9d45f3bb0ba060416eaf049cad5ab513e72795
cd-Roid <giyawe98@gmail.com>
2020-07-07 18:37:18 +0200

    Changed the api Parser to use new api format.

--- api-parser.js
+++ api-parser.js
@@ -1,17 +1,19 @@
 const fetch = require("node-fetch").default;
 
 
-setInterval(getResponse, 1000*10);
+setInterval(getResponse, 1000 * 10);
 
 async function getResponse() {
   try {
     let link = await fetch('http://www.clever-fit.com/de-de/fitnessstudio-in-der-naehe/clever-fit-gummersbach/?type=3002');
-    let response = await link.json()
-    let peopleInGym = response.data.response.countCheckedInCustomer
-    let outMsg = `Es sind ${peopleInGym} Leute am trainieren.`
-    
+    let response = await link.json();
+    let peopleInGym = response.data.loadNumberOfPresentCustomersInPercent;
+    //Theres a max of 42 people allowed in the gym.
+    let calculatePeople = Math.floor(peopleInGym * 42);
+
+    let outMsg = `Es sind ${calculatePeople} Leute am trainieren.`;
     console.log(outMsg);
-    module.exports = outMsg
+    module.exports = outMsg;
   } catch (error) {
     console.error(error);
   }
